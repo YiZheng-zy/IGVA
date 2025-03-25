bb3544678bf40d4eff09c53749f2172c803474e1
YiZheng-zy <84223364+YiZheng-zy@users.noreply.github.com>
2025-03-25 10:17:38 +0800

    Delete scripts/v1_5/finetune_debug.sh

--- scripts/v1_5/finetune_debug.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# make sure that per_device_train_batch_size * gradient_accumulation_steps * num_gpus = 128
-export WANDB_MODE=disabled
-# export TOKENIZERS_PARALLELISM=false
-deepspeed --include=localhost:0,1,2,3 --master_port 29501 llava/train/train_mem.py \
-    --deepspeed scripts/zero3.json \
-    --output_dir llava-v1.5-7b-finetune \
-   
